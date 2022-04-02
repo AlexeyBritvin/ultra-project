8b9131cfaa4d913115f4a41a45ae2f383de9db62
Aleksey <krugombeda@gmail.com>
2022-04-02 21:14:00 +0400

    feat: implement search gifs

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, iif, shareReplay, startWith, switchMap } from 'rxjs';
 import { GiphyService } from '../../services';
 
 @Component({
@@ -8,7 +10,21 @@ import { GiphyService } from '../../services';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
+  search: FormControl = new FormControl();
+
   trending$ = this.giphyService.getTrending();
+  searchValue$ = this.search.valueChanges.pipe(
+    debounceTime(300),
+    shareReplay()
+  );
+  searchGifs$ = this.searchValue$.pipe(
+    switchMap((value) => this.giphyService.searchGifs(value))
+  );
+
+  gifs$ = this.searchValue$.pipe(
+    startWith(null),
+    switchMap((value) => iif(() => !!value, this.searchGifs$, this.trending$))
+  );
 
   constructor(private giphyService: GiphyService) {}
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GiphyItemComponent } from './components/giphy-item/giphy-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DashboardComponent, GiphyItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [DashboardComponent],
 })
 export class DashboardModule {}
